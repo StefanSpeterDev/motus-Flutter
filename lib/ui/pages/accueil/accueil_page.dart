0b26dcbea82d91e1325073f27b0d4d799a5d9d4d
One-cy <plancke.cyril@gmail.com>
2022-06-07 11:05:51 +0200

    add dark theme switcher

--- lib/ui/pages/accueil/accueil_page.dart
+++ lib/ui/pages/accueil/accueil_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../main.dart';
+
 class AccueilPage extends StatefulWidget {
   const AccueilPage({Key? key, required this.title}) : super(key: key);
 
@@ -10,18 +12,29 @@ class AccueilPage extends StatefulWidget {
 }
 
 class _AccueilPageState extends State<AccueilPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the AccueilPage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Text('Accueil'),
-      )// This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: AppBar(
+          // Here we take the value from the AccueilPage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+                icon: Icon(MyApp.themeNotifier.value == ThemeMode.light
+                    ? Icons.dark_mode
+                    : Icons.light_mode),
+                onPressed: () {
+                  MyApp.themeNotifier.value =
+                      MyApp.themeNotifier.value == ThemeMode.light
+                          ? ThemeMode.dark
+                          : ThemeMode.light;
+                })
+          ],
+        ),
+        body: Container(
+          child: Text('Accueil'),
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
